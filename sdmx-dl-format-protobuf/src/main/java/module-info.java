0f55e25f9e659fa6f4d47d997ed76d0bba155acf
Philippe Charles <philippe.charles@nbb.be>
2022-10-21 12:24:16 +0200

    Add protobuf as cache backend

--- sdmx-dl-format-protobuf/src/main/java/module-info.java
+++ sdmx-dl-format-protobuf/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module sdmxdl.format.protobuf {
 
     requires static lombok;
+    requires static nbbrd.service;
     requires static org.checkerframework.checker.qual;
 
     requires transitive sdmxdl.format.util;
     requires transitive com.google.protobuf;
 
     exports sdmxdl.format.protobuf;
+
+    provides sdmxdl.format.spi.FileFormatProvider with internal.sdmxdl.format.protobuf.ProtobufProvider;
 }
