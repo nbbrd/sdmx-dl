eefbb02eb35131964fb21d50a5bc19b335dd3cb5
Philippe Charles <philippe.charles@nbb.be>
2024-09-10 10:03:22 +0200

    Add generation of About class from Maven properties

---
+++ sdmx-dl-api/src/main/java-templates/sdmxdl/About.java
@@ -0,0 +1,12 @@
+package sdmxdl;
+
+public final class About {
+
+    private About() {
+        // static class
+    }
+
+    public static final String NAME = "sdmx-dl";
+
+    public static final String VERSION = "${project.version}";
+}
