3703cebf3c09cec6d4dd5de532fe362f72d7f965
Philippe Charles <philippe.charles@nbb.be>
2024-09-24 13:50:03 +0200

    Fix user-agent test

--- sdmx-dl-provider-ri/src/test/java/sdmxdl/provider/ri/drivers/RiHttpUtilsTest.java
+++ sdmx-dl-provider-ri/src/test/java/sdmxdl/provider/ri/drivers/RiHttpUtilsTest.java
@@ -43,14 +43,8 @@ public class RiHttpUtilsTest {
         assertThat(RiHttpUtils.newContext(source, DriverAssert.noOpWebContext()).getUserAgent())
                 .startsWith("sdmx-dl/");
 
-        String previous = System.setProperty(DriverProperties.USER_AGENT_PROPERTY.getKey(), "hello world");
-        try {
-            assertThat(RiHttpUtils.newContext(source, DriverAssert.noOpWebContext()).getUserAgent())
-                    .startsWith("hello world");
-        } finally {
-            if (previous != null)
-                System.setProperty(DriverProperties.USER_AGENT_PROPERTY.getKey(), previous);
-        }
+        assertThat(RiHttpUtils.newContext(source.toBuilder().property(DriverProperties.USER_AGENT_PROPERTY.getKey(), "hello world").build(), DriverAssert.noOpWebContext()).getUserAgent())
+                .startsWith("hello world");
     }
 
     @Test
