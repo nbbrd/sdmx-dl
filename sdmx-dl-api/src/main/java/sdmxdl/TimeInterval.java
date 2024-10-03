c3d8a0e5d7c4a80dae0232a2eac7bdd26959d7a4
Philippe Charles <philippe.charles@nbb.be>
2024-10-03 09:21:08 +0200

    Improve TimeInterval

--- sdmx-dl-api/src/main/java/sdmxdl/TimeInterval.java
+++ sdmx-dl-api/src/main/java/sdmxdl/TimeInterval.java
@@ -1,6 +1,7 @@
 package sdmxdl;
 
 import lombok.NonNull;
+import nbbrd.design.MightBePromoted;
 import nbbrd.design.RepresentableAsString;
 import nbbrd.design.StaticFactoryMethod;
 
@@ -13,7 +14,8 @@ import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 /**
- * Simplified implementation of ISO-8601 time intervals that uses the <code>start/duration</code> expression.
+ * Simplified implementation of <a href="https://en.wikipedia.org/wiki/ISO_8601#Time_intervals">ISO-8601 time intervals</a>
+ * that uses the <code>start/duration</code> expression such as <code>2007-03-01T13:00:00Z/P1Y2M10DT2H30M</code>.
  */
 @RepresentableAsString
 @lombok.Value(staticConstructor = "of")
@@ -22,7 +24,7 @@ public class TimeInterval {
     @StaticFactoryMethod
     public static @NonNull TimeInterval parse(@NonNull CharSequence text) throws DateTimeParseException {
         String textAsString = text.toString();
-        int index = textAsString.indexOf(SEPARATOR);
+        int index = textAsString.indexOf(SOLIDUS);
         if (index == -1) {
             throw new DateTimeParseException("Cannot parse time interval", text, 0);
         }
@@ -32,68 +34,94 @@ public class TimeInterval {
         );
     }
 
-    @NonNull LocalDateTime start;
+    @NonNull
+    LocalDateTime start;
 
-    @NonNull Duration duration;
+    @NonNull
+    Duration duration;
 
     @Override
     public String toString() {
-        return start.toString() + SEPARATOR + duration;
+        return start.toString() + SOLIDUS + duration;
     }
 
     public @NonNull String toShortString() {
-        return getStartAsShortString() + SEPARATOR + duration;
+        return getStartAsShortString() + SOLIDUS + duration;
     }
 
+    /**
+     * Gets the start part of this time interval as a short string using
+     * the <a href="https://en.wikipedia.org/wiki/ISO_8601#Reduced_precision">ISO_8601 reduced precision</a> mechanism.
+     *
+     * @return a non-null string
+     */
     public @NonNull String getStartAsShortString() {
-        ChronoUnit minChronoUnit = getMinChronoUnit(duration);
-        int index = Math.max(getMinIndex(minChronoUnit), getDefaultingIndex(start));
-        return start.toString().substring(0, index);
-    }
+        if (start.getNano() != 0) {
+            return start.toString();
+        }
 
-    private static int getMinIndex(ChronoUnit unit) {
-        switch (unit) {
+        int year = start.getYear();
+        int month = start.getMonthValue();
+        int day = start.getDayOfMonth();
+        int hour = start.getHour();
+        int minute = start.getMinute();
+        int second = start.getSecond();
+
+        ChronoUnit startUnit = getMinChronoUnit(second, minute, hour, day, month);
+        ChronoUnit durationUnit = duration.getMinChronoUnit();
+
+        ChronoUnit min = min(startUnit, durationUnit);
+
+        // FIXME: is this the best behavior ?
+        if (second == 0 && min.equals(ChronoUnit.SECONDS)) {
+            min = ChronoUnit.MINUTES;
+        }
+
+        switch (min) {
             case SECONDS:
-                return 19;
+                return year +
+                        (month < 10 ? "-0" : "-") + month +
+                        (day < 10 ? "-0" : "-") + day +
+                        (hour < 10 ? "T0" : "T") + hour +
+                        (minute < 10 ? ":0" : ":") + minute +
+                        (second < 10 ? ":0" : ":") + second;
             case MINUTES:
-                return 16;
+                return year +
+                        (month < 10 ? "-0" : "-") + month +
+                        (day < 10 ? "-0" : "-") + day +
+                        (hour < 10 ? "T0" : "T") + hour +
+                        (minute < 10 ? ":0" : ":") + minute;
             case HOURS:
-                return 13;
+                return year +
+                        (month < 10 ? "-0" : "-") + month +
+                        (day < 10 ? "-0" : "-") + day +
+                        (hour < 10 ? "T0" : "T") + hour;
             case DAYS:
-                return 10;
+                return year +
+                        (month < 10 ? "-0" : "-") + month +
+                        (day < 10 ? "-0" : "-") + day;
             case MONTHS:
-                return 7;
+                return year +
+                        (month < 10 ? "-0" : "-") + month;
             case YEARS:
-                return 4;
+                return String.valueOf(year);
             default:
-                return 23;
+                return start.toString();
         }
     }
 
-    private static int getDefaultingIndex(LocalDateTime start) {
-        if (start.getNano() != 0)
-            return 23;
-        if (start.getSecond() != 0)
-            return 19;
-        if (start.getMinute() != 0)
-            return 16;
-        if (start.getHour() != 0)
-            return 13;
-        if (start.getDayOfMonth() != 1)
-            return 10;
-        if (start.getMonthValue() != 1)
-            return 7;
-        return 4;
+    @MightBePromoted
+    private static <T extends Comparable<T>> T min(T first, T second) {
+        return first.compareTo(second) < 0 ? first : second;
     }
 
-    private static ChronoUnit getMinChronoUnit(Duration duration) {
-        return duration.getUnits().stream()
-                .filter(o -> duration.get(o) != 0)
-                .filter(ChronoUnit.class::isInstance)
-                .map(ChronoUnit.class::cast)
-                .sorted()
-                .findFirst()
-                .orElse(ChronoUnit.FOREVER);
+    private static ChronoUnit getMinChronoUnit(int second, int minute, int hour, int day, int month) {
+        if (second != 0) return ChronoUnit.SECONDS;
+        if (minute != 0) return ChronoUnit.MINUTES;
+        if (hour != 0) return ChronoUnit.HOURS;
+        if (day != 1) return ChronoUnit.DAYS;
+        if (month != 1) return ChronoUnit.MONTHS;
+        return ChronoUnit.YEARS;
     }
 
     private static final DateTimeFormatter AT_LEAST_YEAR = new DateTimeFormatterBuilder()
@@ -107,5 +135,5 @@ public class TimeInterval {
             .parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
             .toFormatter(Locale.ROOT);
 
-    private static final char SEPARATOR = '/';
+    private static final char SOLIDUS = '/';
 }
