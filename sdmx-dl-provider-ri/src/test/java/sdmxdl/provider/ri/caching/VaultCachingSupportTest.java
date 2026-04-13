1afca3d09f490e59cd2b27a98f3d2c6120923cc5
Philippe Charles <philippe.charles@nbb.be>
2026-04-13 08:59:08 +0200

    Fix flaky test

--- sdmx-dl-provider-ri/src/test/java/sdmxdl/provider/ri/caching/VaultCachingSupportTest.java
+++ sdmx-dl-provider-ri/src/test/java/sdmxdl/provider/ri/caching/VaultCachingSupportTest.java
@@ -4,12 +4,14 @@ import internal.util.credentials.MockedVaultService;
 import org.junit.jupiter.api.Test;
 import sdmxdl.ext.Cache;
 import sdmxdl.web.Credentials;
+import tests.sdmxdl.ext.FakeClock;
 
+import java.time.Clock;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +21,8 @@ class VaultCachingSupportTest {
     public void testGet() throws InterruptedException {
         Duration ttl = Duration.ofMillis(1000);
         Duration evictionDelay = Duration.ofMillis(10);
+        FakeClock clock = new FakeClock();
+        FakeScheduledExecutorService executorService = new FakeScheduledExecutorService(clock);
 
         ConcurrentMap<String, Credentials> dryValues = new ConcurrentHashMap<>();
 
@@ -34,6 +38,8 @@ class VaultCachingSupportTest {
                 .evictionDelay(evictionDelay)
                 .vaultService(vaultService)
                 .dryValues(dryValues)
+                .clock(clock)
+                .cleaner(executorService)
                 .build();
 
         Cache<Credentials> cache = x.getCredentialsCache(ttl, null, null);
@@ -46,7 +52,8 @@ class VaultCachingSupportTest {
         assertThat(dryValues).containsKey(key.getResource());
         assertThat(vaultService.getLoadCount()).isEqualTo(1);
 
-        Thread.sleep(evictionDelay.toMillis() * 2);
+        clock.plus(evictionDelay.toMillis() * 2);
+        executorService.apply();
         assertThat(dryValues).containsKey(key.getResource());
         assertThat(cache.get(key.getResource()))
                 .as("Non-expired key should return value")
@@ -54,7 +61,8 @@ class VaultCachingSupportTest {
         assertThat(dryValues).containsKey(key.getResource());
         assertThat(vaultService.getLoadCount()).isEqualTo(1);
 
-        Thread.sleep(ttl.toMillis() + 1);
+        clock.plus(ttl.toMillis() + 1);
+        executorService.apply();
         assertThat(dryValues).isEmpty();
         assertThat(cache.get(key.getResource()))
                 .as("Expired key should return new value")
@@ -65,5 +73,118 @@ class VaultCachingSupportTest {
 
     private static char[] toPassword(Credentials credentials) {
         return credentials != null ? credentials.getCredentials().getPassword() : null;
+    }
+
+    @lombok.RequiredArgsConstructor
+    @SuppressWarnings("NullableProblems")
+    private static final class FakeScheduledExecutorService implements ScheduledExecutorService {
+
+        @lombok.Data
+        private static class Item {
+            Instant nextTime;
+            Runnable command;
+            Duration delay;
+        }
+
+        private final Clock clock;
+        private final List<Item> scheduledCommands = new ArrayList<>();
+
+        public void apply() {
+            scheduledCommands
+                    .forEach(item -> {
+                        if (clock.instant().isAfter(item.getNextTime())) {
+                            item.command.run();
+                            item.setNextTime(item.getNextTime().plus(item.getDelay()));
+                        }
+                    });
+        }
+
+        @Override
+        public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+            return null;
+        }
+
+        @Override
+        public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+            return null;
+        }
+
+        @Override
+        public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+            return null;
+        }
+
+        @Override
+        public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+            Item item = new Item();
+            item.command = command;
+            item.nextTime = Instant.now().plus(Duration.ofMillis(initialDelay));
+            item.delay = Duration.of(unit.convert(delay, TimeUnit.MILLISECONDS), ChronoUnit.MILLIS);
+            scheduledCommands.add(item);
+            return null;
+        }
+
+        @Override
+        public void shutdown() {
+        }
+
+        @Override
+        public List<Runnable> shutdownNow() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public boolean isShutdown() {
+            return false;
+        }
+
+        @Override
+        public boolean isTerminated() {
+            return false;
+        }
+
+        @Override
+        public boolean awaitTermination(long timeout, TimeUnit unit) {
+            return false;
+        }
+
+        @Override
+        public <T> Future<T> submit(Callable<T> task) {
+            return null;
+        }
+
+        @Override
+        public <T> Future<T> submit(Runnable task, T result) {
+            return null;
+        }
+
+        @Override
+        public Future<?> submit(Runnable task) {
+            return null;
+        }
+
+        @Override
+        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public <T> T invokeAny(Collection<? extends Callable<T>> tasks) {
+            return null;
+        }
+
+        @Override
+        public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
+            return null;
+        }
+
+        @Override
+        public void execute(Runnable command) {
+        }
     }
 }
